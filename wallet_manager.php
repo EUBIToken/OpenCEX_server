afa3f3466b1510a4b0c269dfb7967c5390c9306e
Jessie Lesbian <jessielesbian@protonmail.com>
2022-02-21 16:16:38 +0700

    Update wallet_manager.php

--- wallet_manager.php
+++ wallet_manager.php
@@ -131,7 +131,7 @@ final class OpenCEX_native_token extends OpenCEX_token{
 		}, $this->manager->address)[1];
 		$remains = $chainquotes[2]->sub($chainquotes[1]->mul(OpenCEX_uint::init($this->ctx, "21000")), "Amount not enough to cover blockchain fee!");
 		$this->ctx->check_safety(array_key_exists($this->manager->chainid, OpenCEX_chainids), "Invalid chainid!");
-		file_get_contents(implode(["https://opencex-dev-worker.herokuapp.com/", urlencode(strval(getenv("OpenCEX_shared_secret"))), "/sendAndCreditWhenSecure/", OpenCEX_chainids[$this->manager->chainid], "/", $this->manager->signTransactionIMPL(new OpenCEX_Ethereum_Transaction($chainquotes[0]->tohex(), $chainquotes[1]->tohex(), "0x5208", $this->manager->reconstruct()->address, $remains->tohex())), "/", strval($from), "/", $this->name, "/", strval($remains)]));
+		file_get_contents(implode([((getenv('OpenCEX_devserver') === "true") ? "https://opencex-dev-worker.herokuapp.com/" : "https://opencex-prod-worker.herokuapp.com/"), urlencode(strval(getenv("OpenCEX_shared_secret"))), "/sendAndCreditWhenSecure/", OpenCEX_chainids[$this->manager->chainid], "/", $this->manager->signTransactionIMPL(new OpenCEX_Ethereum_Transaction($chainquotes[0]->tohex(), $chainquotes[1]->tohex(), "0x5208", $this->manager->reconstruct()->address, $remains->tohex())), "/", strval($from), "/", $this->name, "/", strval($remains)]));
 	}
 }
 ?>
