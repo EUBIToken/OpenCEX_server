7b6d3ff79343e93a983b46036b626d7150afbee2
Jessie Lesbian <jessielesbian@protonmail.com>
2022-02-21 08:55:47 +0700

    Ready for production!

--- htdocs/index.php
+++ htdocs/index.php
@@ -568,7 +568,7 @@ try{
 	$leaked_ctx = null;
 	$ctx->finish_emitting();
 } catch (OpenCEX_assert_exception $e){
-	die('{"status": "error", "reason": "' . escapeJsonString($e) . '"}');
+	die('{"status": "error", "reason": "' . escapeJsonString($e->getMessage()) . '"}');
 } catch (Exception $e){
 	//NOTE: if we fail due to unexpected exception, we must destroy the context!
 	//Automatic context destruction only occours for OpenCEX_assert_exceptions.
@@ -576,7 +576,7 @@ try{
 		$leaked_ctx->destroy();
 	}
 		
-	$fail_message = '{"status": "error", "reason": "Unexpected internal server error: ' . escapeJsonString($e) . '"}';
+	$fail_message = '{"status": "error", "reason": "Unexpected internal server error: ' . escapeJsonString($e->getMessage()) . '"}';
 	die($fail_message);
 }
 
